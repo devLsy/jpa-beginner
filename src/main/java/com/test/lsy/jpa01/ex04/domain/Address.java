c5e41a42af1dca9687649fef60516b924314ecb4
이상엽 <infightersy@naver.com>
2025-04-29 16:40:08 +0900

    값 타입 클래스 불변으로 수정, equals and hashcode 추가

--- src/main/java/com/test/lsy/jpa01/ex04/domain/Address.java
+++ src/main/java/com/test/lsy/jpa01/ex04/domain/Address.java
@@ -2,13 +2,36 @@ package com.test.lsy.jpa01.ex04.domain;
 
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Objects;
 
 // 값 타입
 @Embeddable
-@Getter @Setter
+@Getter
+@RequiredArgsConstructor
 public class Address {
-    private String addr1;
-    private String addr2;
-    private String zipcode;
+
+    private final String addr1;
+    private final String addr2;
+    private final String zipcode;
+
+    protected Address() {
+        this.addr1 = null;
+        this.addr2 = null;
+        this.zipcode = null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(addr1, address.addr1) && Objects.equals(addr2, address.addr2) && Objects.equals(zipcode, address.zipcode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(addr1, addr2, zipcode);
+    }
 }
