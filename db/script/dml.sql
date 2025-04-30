3b609c5b135d67d70abaf43923793de1e984f899
이상엽 <infightersy@naver.com>
2025-04-30 16:05:41 +0900

    1:1 단방향 연관 테스트 완료, N + 1도 재연 완료, dml 추가

---
+++ db/script/dml.sql
@@ -0,0 +1,27 @@
+INSERT INTO user10
+(email, create_date, name)
+VALUES('test@example.com', '2025-04-30 15:21:54.085745', '테스트 유저');
+INSERT INTO user10
+(email, create_date, name)
+VALUES('test1@example.com', '2025-04-30 15:29:29.863130', '홍길동');
+INSERT INTO user10
+(email, create_date, name)
+VALUES('test2@example.com', '2025-04-30 15:29:33.047869', '홍길순');
+INSERT INTO user10
+(email, create_date, name)
+VALUES('test3@example.com', '2025-04-30 15:29:39.071954', '홍길복');
+INSERT INTO user10
+(email, create_date, name)
+VALUES('test4@example.com', '2025-04-30 15:29:44.793052', '타노스');
+
+INSERT INTO membership_card
+(card_no, user_email)
+VALUES('CARD-001', 'test@example.com');
+
+INSERT INTO membership_card
+(card_no, user_email)
+VALUES('CARD-002', 'test1@example.com');
+
+INSERT INTO membership_card
+(card_no, user_email)
+VALUES('CARD-003', 'test2@example.com');
